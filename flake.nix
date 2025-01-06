07987171ead7e4a0e7f1e2c3fc4ff3ba16c098bb
Sirui Lu <siruilu@cs.washington.edu>
2025-01-05 18:39:23 -0800

    :sparkles: Initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Build environment for grisette";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        lib = import ./.;
+      }
+    );
+}
