1e16613160a6d45800f89afedcb81b6d57eef615
Sirui Lu <siruilu@cs.washington.edu>
2025-01-05 20:06:10 -0800

    :pushpin: Fix cairo dependencies below or equal to GHC 928

--- hpkgs-902.nix
+++ hpkgs-902.nix
@@ -3,5 +3,6 @@ with pkgs.haskell.lib;
 (import ./hpkgs-base.nix { inherit pkgs; ghcVersion = "902"; }).extend (
   hfinal: hprev: {
     sbv = dontCheck (hfinal.callHackage "sbv" "9.2" { });
+    Chart-cairo = hfinal.callHackage "Chart-cairo" "1.9.3" { };
   }
 )
