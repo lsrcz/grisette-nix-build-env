f7a5e9842f8c669105845c0b1832c1b659c97d7f
Sirui Lu <siruilu@cs.washington.edu>
2025-07-03 00:38:44 -0700

    update hackage snapshot

--- hpkgs-base.nix
+++ hpkgs-base.nix
@@ -3,7 +3,7 @@ pkgs.haskell.packages."ghc${ghcVersion}".override (prev: {
   all-cabal-hashes = pkgs.fetchFromGitHub {
     owner = "commercialhaskell";
     repo = "all-cabal-hashes";
-    rev = "eb6641d2dfddf269c90c2cfdec94b3f1145418f3";
-    sha256 = "sha256-hiL/IIoCISoPXq9OviWR7UMQF7RK4WukGRNP+ht4gpw=";
+    rev = "8644ed27bb9149ea18ea46f59ecfaf1edb83fdeb";
+    sha256 = "sha256-qd4n7LttO5GjWTVuwKKM5r0cqB9qD+fkXCkoSYc0hhg=";
   };
 })
